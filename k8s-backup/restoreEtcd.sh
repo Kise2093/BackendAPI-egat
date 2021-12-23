1dbf26f0ac8e94620e646f230c1bd71536bccf53
Kris' Big Boy <people12337@gmail.com>
2021-12-23 14:40:34 +0700

    restore

---
+++ k8s-backup/restoreEtcd.sh
@@ -0,0 +1,9 @@
+BACKUP_PATH=/home/ptwop/etcd-backup
+
+
+FILE_NAME=$(find $BACKUP_PATH -type f -exec stat -c '%Y %n' {} \; | sort -nr | awk 'NR==1 {print $2}')
+LATEST_FILE_NAME=$(echo "$FILE_NAME" | awk -F/ '{print $2}')
+
+echo $LATEST_FILE_NAME
+
+#etcdctl --endpoints 10.40.58.160:2379 snapshot restore snapshotdb
