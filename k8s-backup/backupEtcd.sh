a91b00c0369e427f7baa3341cda6ac4bac470996
Kris' Big Boy <people12337@gmail.com>
2021-12-23 13:37:57 +0700

    fix

--- k8s-backup/backupEtcd.sh
+++ k8s-backup/backupEtcd.sh
@@ -11,7 +11,7 @@ echo "Current Time : $CURRENT_TIME"
 FILE_NAME="$BASE_FILE_NAME.$CURRENT_TIME.db"
  echo "Back up to the following file: " "$FILE_NAME"
 
-etcdctl --endpoints 10.40.58.160:2379 --cert=/etc/kubernetes/pki/etcd/server.crt --key=/etc/kubernetes/pki/etcd/server.key --cacert=/etc/kubernetes/pki/etcd/ca.crt snapshot save $FILE_NAME
+/usr/local/bin/etcdctl --endpoints 10.40.58.160:2379 --cert=/etc/kubernetes/pki/etcd/server.crt --key=/etc/kubernetes/pki/etcd/server.key --cacert=/etc/kubernetes/pki/etcd/ca.crt snapshot save $FILE_NAME
 
 #sudo mkdir -p /tmp/backup/etcd
 #sudo mv /tmp/etcd.$CURRENT_TIME.db /tmp/backup/etcd/etcd.$CURRENT_TIME.db
