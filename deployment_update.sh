50edf59ad4c5b8d0a2d9d8fc5f9b3bfbe2f21f1e
Kise2093 <people12337@gmail.com>
2021-12-24 01:01:57 +0700

    yeah

--- deployment_update.sh
+++ deployment_update.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-NUMBER_OF_API=$(cat test.txt | awk '{print NF}');
+echo $(find backend-api -type f -name '*-deployment.yaml' | sed 's/ / -f /') > tmp_find_deployment.txt
 
-echo "kubectl -n erc -p2p \\" > test_deployment.sh
+
+NUMBER_OF_API=$(cat tmp_find_deployment.txt | awk '{print NF}');
+
+echo "kubectl -n erc -p2p \\" > tmp_deployment_script.sh
 for i in  $(seq 1 $NUMBER_OF_API)
 do 
     if (($i < 16))
     then
-        echo "$(cat test.txt | awk -v i="$i" '{print $i}') \\" >> test_deployment.sh
+        echo "$(cat tmp_find_deployment.txt | awk -v i="$i" '{print $i}') \\" >> tmp_deployment_script.sh
         #echo $(echo $i) >> test.sh
     else
-        echo "$(cat test.txt | awk -v i="$i" '{print $i}')" >> test_deployment.sh
+        echo "$(cat tmp_find_deployment.txt | awk -v i="$i" '{print $i}')" >> tmp_deployment_script.sh
     fi
 done
 
-bash test_deployment.sh
+#bash tmp_deployment_script.sh
