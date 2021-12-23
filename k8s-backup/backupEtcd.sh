950a2348d9a5dd54f1925ffee5790ddd8488d2e9
Kris' Big Boy <people12337@gmail.com>
2021-12-23 13:50:22 +0700

    fully functional cronjob backup k8s

--- k8s-backup/backupEtcd.sh
+++ k8s-backup/backupEtcd.sh
@@ -9,18 +9,15 @@ CURRENT_TIME=$(date "+%Y.%m.%d-%H.%M.%S")
 echo "Current Time : $CURRENT_TIME"
 
 FILE_NAME="$BASE_FILE_NAME.$CURRENT_TIME.db"
- echo "Back up to the following file: " "$FILE_NAME"
+echo "Back up to the following file: " "$FILE_NAME"
 
 /usr/local/bin/etcdctl --endpoints 10.40.58.160:2379 --cert=/etc/kubernetes/pki/etcd/server.crt --key=/etc/kubernetes/pki/etcd/server.key --cacert=/etc/kubernetes/pki/etcd/ca.crt snapshot save $FILE_NAME
 
 #sudo mkdir -p /tmp/backup/etcd
 #sudo mv /tmp/etcd.$CURRENT_TIME.db /tmp/backup/etcd/etcd.$CURRENT_TIME.db
 
-#TODO delete file older than 7 days 
-#write echo what file was deleted
+#find file that have been modified or create more than 7 day then delete it
 
-find $BACKUP_PATH -cmin +10 -type f -delete
+#find $BACKUP_PATH -cmin +10 -type f -delete
 
-#find $BACKUP_PATH -mtime +7 -type f -delete
-
-echo "script finish at: $CURRENT_TIME"
+find $BACKUP_PATH -mtime +7 -type f -delete
