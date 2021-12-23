297bfafa13826dca13429c5b7e50959f0d8c9284
Kris' Big Boy <people12337@gmail.com>
2021-12-23 15:05:43 +0700

    final restore

--- k8s-backup/restoreEtcd.sh
+++ k8s-backup/restoreEtcd.sh
@@ -4,12 +4,12 @@ BACKUP_PATH=/home/ptwop/etcd-backup
 FILE_NAME=$(find $BACKUP_PATH -type f -exec stat -c '%Y %n' {} \; | sort -nr | awk 'NR==1 {print $2}')
 LATEST_FILE_NAME=$(echo "$FILE_NAME" | awk -F/ '{print $5}')
 
-echo $LATEST_FILE_NAME
+#cho $LATEST_FILE_NAME
 
 while true; do
     read -p "Do you wish to restore etcd cluster using $LATEST_FILE_NAME ? [Y/n]" yn
     case $yn in
-        [Yy]* ) echo $LATEST_FILE_NAME ; break;;
+        [Yy]* ) etcdctl --endpoints 10.40.58.160:2379 snapshot $FILE_NAME ; break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
