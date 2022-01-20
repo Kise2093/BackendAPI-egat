8c5245af088a4c29458ed4ef3d322aba760ee793
Kris sassy <people12337@gmail.com>
2022-01-21 00:51:38 +0700

    1.6.1-beta

--- tmp_deployment_script.sh
+++ tmp_deployment_script.sh
@@ -1,18 +1,27 @@
 kubectl -n erc -p2p \
-./backend-api/bilateral-api/app-deployment.yaml \
-./backend-api/egat-p2p-api/egat-p2p-api-deployment.yaml \
-./backend-api/forecast-api/app-deployment.yaml \
-./backend-api/meter-api/egat-p2p-meter-api-deployment.yaml \
-./backend-api/mobile-login-api/egat-p2p-mobile-login-api-deployment.yaml \
-./backend-api/mobile-register-api/egat-p2p-mobile-register-api-deployment.yaml \
-./backend-api/otp-gateway-api/egat-p2p-otp-gateway-api-deployment.yaml \
-./backend-api/pool-api/app-deployment.yaml \
-./backend-api/power-api/app-deployment.yaml \
-./backend-api/power-job-api/app-deployment.yaml \
-./backend-api/profile-management-api/egat-p2p-mobile-profile-api-deployment.yaml \
-./backend-api/refdata-hyperledger-api/egat-p2p-reference-data-hyperledger-api-deployment.yaml \
-./backend-api/trade-api/app-deployment.yaml \
-./backend-api/trading-job-api/app-deployment.yaml \
-./backend-api/web-admin-login-api/egat-p2p-web-admin-login-api-deployment.yaml \
-./backend-api/web-admin-user-management-api/egat-p2p-web-admin-user-management-api-deployment.yaml
 ./frontend/cpram-foodtrace-frontend-deployment.yaml
+./backend-api/trading-job-api/app-deployment.yaml
+./backend-api/notification-api/app-deployment.yaml
+./backend-api/power-api/app-deployment.yaml
+./backend-api/web-admin-login-api/egat-p2p-web-admin-login-api-deployment.yaml
+./backend-api/mobile-login-api/egat-p2p-mobile-login-api-deployment.yaml
+./backend-api/power-job-api/app-deployment.yaml
+./backend-api/mobile-register-api/egat-p2p-mobile-register-api-deployment.yaml
+./backend-api/egat-p2p-api/egat-p2p-api-deployment.yaml
+./backend-api/refdata-hyperledger-api/egat-p2p-reference-data-hyperledger-api-deployment.yaml
+./backend-api/forecast-api/app-deployment.yaml
+./backend-api/otp-gateway-api/egat-p2p-otp-gateway-api-deployment.yaml
+./backend-api/pool-api/app-deployment.yaml
+./backend-api/egat-p2p-web-admin-ref-management-api/egat-p2p-web-admin-ref-management-api-deployment.yaml
+./backend-api/trade-api/app-deployment.yaml
+./backend-api/profile-management-api/egat-p2p-mobile-profile-api-deployment.yaml
+./backend-api/egat-p2p-web-admin-summary-report-api/egat-p2p-web-admin-summary-report-api-deployment.yaml
+./backend-api/egat-p2p-mobile-report-api/app-deployment.yaml
+./backend-api/web-admin-user-management-api/egat-p2p-web-admin-user-management-api-deployment.yaml
+./backend-api/bilateral-api/app-deployment.yaml
+./backend-api/news-api/egat-p2p-web-admin-news-management-api-deployment.yaml
+./backend-api/meter-api/egat-p2p-meter-api-deployment.yaml
+./smartcontract/smartcontract-api/egat-p2p-trade-api-smartcontract/egat-p2p-mobile-trade-hyperledger-api-deployment.yaml
+./smartcontract/smartcontract-api/egat-p2p-pool-api-smartcontract/egat-p2p-mobile-pool-hyperledger-api-deployment.yaml
+./smartcontract/smartcontract-api/refdata-hyperledger-api/egat-p2p-reference-data-hyperledger-api-deployment.yaml
+./smartcontract/smartcontract-api/egat-p2p-bilateral-api-smartcontract/egat-p2p-mobile-bilateral-hyperledger-api-deployment.yaml
