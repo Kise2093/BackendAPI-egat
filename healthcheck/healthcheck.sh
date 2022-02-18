f73ecf54084b760d4265fb0c168d5149ec6f328b
Kise2093 <people12337@gmail.com>
2022-02-18 15:54:53 +0700

    healthcheck

---
+++ healthcheck/healthcheck.sh
@@ -0,0 +1,9 @@
+ k get pod -A
+ k top node
+ k top pod
+ k -n erc-p2p top po
+ k -n druid top po
+ k -n kafka-connect top po
+ k -n hyperledger top po
+ k -n rook-ceph top po
+ k -n kube-system top po
